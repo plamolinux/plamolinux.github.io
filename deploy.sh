90b2e61562365a97bc11486ac41026dac33746a2
KATOH Yasufumi <karma@jazz.email.ne.jp>
2021-05-04 02:07:07 +0900

    Add deploy.sh for deploying

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "************ Deleting old publication ************"
+rm -rf public
+
+echo "************ Generating site ************"
+hugo
+
+echo "************ Adding CNAME ************"
+echo iriya-ufo.net >> public/CNAME
+
+echo "************ git add && git commit ************"
+git add public/
+git commit -m "release `date '+%Y-%m-%d %H:%M'`"
+
+echo "************ Updating master branch ************"
+git subtree push --prefix public/ origin master
+
+echo "************ Updating source branch ************"
+git push origin source
+
+echo "************ Success Deploy ************"
